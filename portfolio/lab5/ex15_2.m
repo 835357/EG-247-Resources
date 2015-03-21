16cc625fbb5eff1abe03906ca585fa0dbdd2ff69
835357 <835357@swansea.ac.uk>
2015-03-21 00:50:09 +0000

    lab5 incomplete
    lab5 incomplete

--- portfolio/lab5/ex15_2.m
+++ portfolio/lab5/ex15_2.m
@@ -1,20 +1,17 @@
 %% Lab Exercise 15 - Convolution
-% Plot convolution of a piecewise linear function.
+% Plot convolution of a pieewise linear function.
 %
 % Prepared for EG-247 by Chris P. Jobling
-syms h tau t
-h(t) = (-t + 1)*(heaviside(t)-heaviside(t-1);
-u(t) = heaviside(t)+heaviside(t-1);
+syms h u tau t;
+h(t) = exp(-t);    %*(heaviside(t)-heaviside(t-1));
+u(t) = 1;           %*heaviside(t)- heaviside(t-1);
 %% First segment
 % Integration from 0 to t:
-conv1 = int(1*(-tau + 1),tau,0,t)
+conv1(t) = int(h(t-tau)*u(tau),tau,0,t)
 %% Second segment
 % Integration from t - 1 to 1:
-conv2 = int(1*h(tau),tau,t-1,1)
+conv2(t) = int(h(t-tau)*u(tau),tau,t-1,1)
 %% Plot
-t1 = linspace(0,1,100);
-x = -(t1.*(t1 - 2))./2;
-t2 = linspace(1,2,100);
-y = (t2 - 2).^2./2;
-plot(t1,x,t2,y)
+y(t) = conv1(t)*((heaviside(t)-heaviside(t-1))) + conv2(t)*(heaviside(t-1)- heaviside(t-2)) 
+ezplot(y(t), [0,2])
 grid
--- portfolio/lab5/ex15_3.m
+++ portfolio/lab5/ex15_3.m
@@ -1,20 +1,22 @@
 %% Lab Exercise 15 - Convolution
-% Plot convolution of a piecewise linear function.
+% Plot convolution of a pieewise linear function.
 %
 % Prepared for EG-247 by Chris P. Jobling
-syms h tau t
-h(t) = (-t + 1)*(heaviside(t)-heaviside(t-1);
-u(t) = heaviside(t)+heaviside(t-1);
+syms h u tau t;
+h(t) = 2;    %*(heaviside(t)-heaviside(t-1));
+u(t) = 1;           %*heaviside(t)- heaviside(t-2);
 %% First segment
 % Integration from 0 to t:
-conv1 = int(1*(-tau + 1),tau,0,t)
+conv1(t) = int(2,tau,0,t)
 %% Second segment
-% Integration from t - 1 to 1:
-conv2 = int(1*h(tau),tau,t-1,1)
+% Integration from 1 to 2:
+conv2(t) = int(2,tau,1,2)
+
+%% Third segment
+% Integration from t - 2 to 2:
+conv3(t) = int(2,tau,t-1,2)
+
 %% Plot
-t1 = linspace(0,1,100);
-x = -(t1.*(t1 - 2))./2;
-t2 = linspace(1,2,100);
-y = (t2 - 2).^2./2;
-plot(t1,x,t2,y)
+y(t) = conv1(t)*((heaviside(t)-heaviside(t-1))) + conv2(t)*(heaviside(t-1)- heaviside(t-2)) + conv3(t)*(heaviside(t-2)- heaviside(t-3))
+ezplot(y(t), [0,4])
 grid
